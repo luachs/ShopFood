f4186129e12d9d4b6add0ffb591ff72125901c55
Phat <nguyenthanhphat13032005@gmail.com>
2025-08-18 22:09:43 +0700

    backend #2 delay

--- backend/index.js
+++ backend/index.js
@@ -30,8 +30,18 @@ app.post("/upload", upload.single("product"), (req, res) => {
   });
 });
 app.post("/addproduct", async (req, res) => {
+  let products = await Product.find({});
+  let id;
+  if (products.length > 0) {
+    let last_product_array = products.slice(-1);
+    let last_product = last_product_array[0];
+    id = last_product.id + 1;
+  } else {
+    id = 1;
+  }
+
   const product = new Product({
-    id: req.body.id,
+    id: id,
     name: req.body.name,
     image: req.body.image,
     category: req.body.category,
@@ -46,6 +56,20 @@ app.post("/addproduct", async (req, res) => {
     success: true,
     name: req.body.name,
   });
+});
+app.post("/removeproduct", async (req, res) => {
+  await Product.findOneAndDelete({ id: req.body.id });
+  console.log("removed");
+  res.json({
+    success: "true",
+    name: req.body.name,
+  });
+});
+// create api for getting all products
+app.get("/allproducts", async (req, res) => {
+  let products = await Product.find({}); 
+  console.log(products);
+  res.send(products);
 });
 
 // listen
